bdc228993fb496d9f64f91d1485f84663655281c
Yurii Liubchenko <yl@reface.ai>
2022-12-02 16:45:10 +0200

    upd day2 task1

--- day2/task1.py
+++ day2/task1.py
@@ -1,6 +1,3 @@
-WIN_STAT = 6
-DRAW_STAT = 3
-
 STATS = {
 	'A': 1, 'B': 2, 'C': 3,
 	'X': 1, 'Y': 2, 'Z': 3
@@ -8,20 +5,17 @@ STATS = {
 
 
 def get_round_stats(val1, val2):
-	choice_stat = STATS[val2]
-	if (STATS[val1], STATS[val2]) in ((1, 2), (2, 3), (3, 1)):
-		choice_stat += WIN_STAT
-	elif STATS[val1] == STATS[val2]:
-		choice_stat += DRAW_STAT
-	return choice_stat
+	return (
+		(((((STATS[val1] + 1) % 3) + 3 * (STATS[val1] % 2 == 0)) == STATS[val2]) * 6) +  # win
+		((STATS[val1] == STATS[val2]) * 3) +  # draw
+		STATS[val2]  # lose
+	)
 
 
 with open('data.txt', 'r') as f:
 	data = f.read().split('\n')
 
 print(
-	sum(
-		get_round_stats(*r.split())
-		for index, r in enumerate(data)
-	)
+	sum(get_round_stats(*r.split()) for index, r in enumerate(data))
 )
+
