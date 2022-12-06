19f620814dfdec114f0da7738fae3a7997e5ec07
Yurii Liubchenko <yl@reface.ai>
2022-12-06 11:05:11 +0200

    add day6

---
+++ day6/task2.py
@@ -0,0 +1,18 @@
+with open('data.txt', 'r') as f:
+	data = f.read()
+
+stack = []
+
+for index, d in enumerate(data):
+	try:
+		duplicate_index = stack.index(d)
+	except ValueError:
+		stack.append(d)
+	else:
+		stack = stack[duplicate_index+1:]
+		stack.append(d)
+		continue
+
+	if len(stack) >= 14:
+		print(index + 1)
+		break
